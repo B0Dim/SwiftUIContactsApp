e094a0cde0370954aefe550c55469c98a5f9d73b
B0Dim <bodim@me.com>
2022-02-16 16:54:38 +0300

    textCase fix

--- SwiftUIContactsApp/ContactSectionView.swift
+++ SwiftUIContactsApp/ContactSectionView.swift
@@ -24,6 +24,7 @@ struct ContactSectionView: View {
                         Text(person.email)
                     }
                 }
+                .textCase(.none)
             }
             .navigationBarTitle("Most Viewed")
         }
