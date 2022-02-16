4f51022d1550d663a3dd91534b5999edb0f05f9b
B0Dim <bodim@me.com>
2022-02-16 09:17:59 +0300

    Initial Commit

---
+++ SwiftUIContactsApp/SwiftUIContactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContactsAppApp.swift
+//  SwiftUIContactsApp
+//
+//  Created by  BoDim on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
